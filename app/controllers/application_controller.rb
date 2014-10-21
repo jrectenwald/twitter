40c298604ca0681c84711040e23b1faa81b56067
vanessadean <vanessa.dean@gmail.com>
2014-10-21 17:29:52 -0400

    Tweet MVC no db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ require_relative "../../config/environment"
 class ApplicationController < Sinatra::Application
 
   get '/tweets' do
-    Tweet.new("Vanessa", "My first tweet!!! SO EXCITING OMG!!!")
-    Tweet.new("Vanessa", "My second tweet!!! Still super exciting!!!")
+    # Tweet.new("Vanessa", "My first tweet!!! SO EXCITING OMG!!!")
+    # Tweet.new("Vanessa", "My second tweet!!! Still super exciting!!!")
     @tweets = Tweet.all
-
     erb :tweets
   end
 
   post '/tweets' do
-
+    Tweet.new(params[:user], params[:status])
+    redirect '/tweets'
   end
 end
