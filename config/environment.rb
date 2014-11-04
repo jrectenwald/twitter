9a551da3d0a1354cef256333bbe8bef19f62800d
vanessadean <vanessa.dean@gmail.com>
2014-11-03 20:45:45 -0500

    public folder

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,6 @@ require './app/models/user'
 configure do
   set :public_folder, 'public'
   set :views, 'app/views'
-  enable :sessions
-  set :session_secret, 'fwitter'
 end
 
 configure :development do
