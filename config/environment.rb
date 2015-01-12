1cbe778253ea598501d4c5259d2aac3dfafa1e00
vanessadean <vanessa.dean@gmail.com>
2015-01-12 16:42:45 -0500

    update sql connection in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require './app/models/tweet'
 require './app/models/user'
 
 configure :development do
-  set :database, "sqlite3:///db/database.db"
+  set :database, "sqlite3:db/database.db"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,12 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20141023192917) do
 
-  create_table "tweets", force: true do |t|
+  create_table "tweets", force: :cascade do |t|
     t.string  "status"
     t.integer "user_id"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
   end
